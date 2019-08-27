859b4d9e0380b8d6784fcea815028acad3f9099d
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 16:25:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,8 +11,9 @@ end
 def reduce(source_array, starting_point = 0)
   for element in source_array
    starting_point = yield(element, starting_point)
-  # puts starting_point
   end
   
+  if !starting_point
+    return !!starting_point
   return starting_point
 end
