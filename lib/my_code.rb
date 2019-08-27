2af64f66ea2513d2c0811913b91a1c40a39f5fd3
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 15:58:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(source_array, starting_point = 0)
   total = starting_point
   
   for element in source_array
-    yield(total, element)
+    total = yield(total, element)
   end
   
   return total
