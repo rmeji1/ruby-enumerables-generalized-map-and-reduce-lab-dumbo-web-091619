b105b848a17f613e01ae4335c3acfed1635c081b
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 16:29:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # Your Code Here
+def map(source_array)
+  dest_array = []
+  for element in source_array
+    dest_array << yield(element)
+  end
+  
+  return dest_array
+end
+
+def reduce(source_array, starting_point = 0)
+  for element in source_array
+   starting_point = yield(element, starting_point)
+  end
+  
+  return !starting_point ? !!starting_point : starting_point
+end
